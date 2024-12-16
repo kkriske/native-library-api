3bc316e275e3c324afb879a3f509c3da233050db
Kristof Dhondt <kristofdho@gmail.com>
2024-12-16 23:27:40 +0100

    - remove unpacked jextract

--- jextract/bin/jextract.bat
+++
@@ -1,2 +0,0 @@
-@set ROOT=%~dp0..
-@"%ROOT%\runtime\bin\java" %JEXTRACT_JAVA_OPTIONS% -m org.openjdk.jextract/org.openjdk.jextract.JextractTool %*
--- jextract/bin/jextract.ps1
+++
@@ -1,2 +0,0 @@
-$ROOT = "$PSScriptRoot\.."
-& "$ROOT\runtime\bin\java" $Env:JEXTRACT_JAVA_OPTIONS -m 'org.openjdk.jextract/org.openjdk.jextract.JextractTool' $args
